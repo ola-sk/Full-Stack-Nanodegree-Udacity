34dc7c3ef7def1fbc816a1e17f38cd9f773cb74e
ola-sk <50403262+ola-sk@users.noreply.github.com>
2020-05-06 11:44:34 +0200

    C in CRUD: Create a record in the db and update content on the website asynchronously based on a positive response of the server.
    Operations performed on the app (thanks Flask-SQLAlchemy):
    flask db init
    flask db migrate -m "Initial migration."
    flask db upgrade
    
    At this point assume that we add some records to the database.
    The next migration that will be performed needs to take it into account
    (hand-tuning the migration e.g. if there are constraints on adding the
    column to the db).

---
+++ MODULE_1/todo_app/app.py
@@ -0,0 +1,105 @@
+# —————————————————————————————————————————————————————————————————————————————
+# RUN DEVELOPMENT SERVER
+# —————————————————————————————————————————————————————————————————————————————
+# Enable debug mode with either:
+# $ export FLASK_ENV=development
+# or
+# $ export FLASK_DEBUG=true
+# Exported variables persist per the terminal session.
+# Export prior to running `flask run`. Or call together with `flask run`:
+# 
+# $ FLASK_APP=app.py FLASK_DEBUG=true flask run
+# —————————————————————————————————————————————————————————————————————————————
+# RUNNING THE APP
+# —————————————————————————————————————————————————————————————————————————————
+# 1st way:
+# Terminal: in the directory of app.py
+# $ export FLASK_APP=app.py
+# $ flask run
+# or
+# $ FLASK_APP=app.py flask run
+
+# 2nd way:
+# Append this snippet to the end of the app.py file:
+# 
+# if __name__ == '__main__':
+#   app.run()
+# 
+# and run the file with python:
+# $ python app.py
+# —————————————————————————————————————————————————————————————————————————————
+# SET UP LINTING IN VS CODE
+# —————————————————————————————————————————————————————————————————————————————
+# For MS Code add following to the project's `.vscode/settings.json` to disable pylint yielding errors and warnings on some correct flask and flask-sqlalchemy specific code structuring:
+# 
+# {
+#   "python.linting.pylintArgs": [
+#     "--load-plugins",
+#     "pylint-flask",
+#     "pylint-flask-sqlalchemy",
+#   ],
+# }
+# —————————————————————————————————————————————————————————————————————————————
+# —————————————————————————————————————————————————————————————————————————————
+# APP CONTENT
+# —————————————————————————————————————————————————————————————————————————————
+
+from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
+
+app = Flask(__name__)
+# TODO
+# Manually create postgresql database set up in the app.config['SQLALCHEMY_DATABASE_URI']:
+# 'SQLservertype://username:password@host:port/databasename
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ola:test123@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+# —————————————————————————————————————————————————————————————————————————————
+# MODELS
+# —————————————————————————————————————————————————————————————————————————————
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+# —————————————————————————————————————————————————————————————————————————————
+# HANDLE ROUTES
+# —————————————————————————————————————————————————————————————————————————————
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  error = False
+  body = {}
+  try:
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort (400)
+  else:
+    return jsonify(body)
+
+@app.route('/')
+def index():
+  return render_template('index.html', data=Todo.query.all())
+
+# —————————————————————————————————————————————————————————————————————————————
+# FOR OPTIONAL RUNNING THE APP WITH `PYTHON` COMMAND
+# —————————————————————————————————————————————————————————————————————————————
+if __name__ == '__main__':
+  app.run()
