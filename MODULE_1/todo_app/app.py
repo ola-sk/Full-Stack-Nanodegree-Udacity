d7f45c0c4be467ed37a93f738fe4384ce8aa5d34
ola-sk <50403262+ola-sk@users.noreply.github.com>
2020-05-13 22:06:15 +0200

    Format document.

--- MODULE_1/todo_app/app.py
+++ MODULE_1/todo_app/app.py
@@ -7,7 +7,7 @@
 # $ export FLASK_DEBUG=true
 # Exported variables persist per the terminal session.
 # Export prior to running `flask run`. Or call together with `flask run`:
-# 
+#
 # $ FLASK_APP=app.py FLASK_DEBUG=true flask run
 # —————————————————————————————————————————————————————————————————————————————
 # RUNNING THE APP
@@ -21,17 +21,17 @@
 
 # 2nd way:
 # Append this snippet to the end of the app.py file:
-# 
+#
 # if __name__ == '__main__':
 #   app.run()
-# 
+#
 # and run the file with python:
 # $ python app.py
 # —————————————————————————————————————————————————————————————————————————————
 # SET UP LINTING IN VS CODE
 # —————————————————————————————————————————————————————————————————————————————
 # For MS Code add following to the project's `.vscode/settings.json` to disable pylint yielding errors and warnings on some correct flask and flask-sqlalchemy specific code structuring:
-# 
+#
 # {
 #   "python.linting.pylintArgs": [
 #     "--load-plugins",
@@ -54,7 +54,7 @@ app = Flask(__name__)
 # Manually create postgresql database set up in the app.config['SQLALCHEMY_DATABASE_URI']:
 # 'SQLservertype://username:password@host:port/databasename
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ola:test123@localhost:5432/todoapp'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
@@ -62,115 +62,129 @@ migrate = Migrate(app, db)
 # —————————————————————————————————————————————————————————————————————————————
 # MODELS
 # —————————————————————————————————————————————————————————————————————————————
+
+
 class Todo(db.Model):
-  __tablename__ = 'todos'
-  id = db.Column(db.Integer, primary_key=True)
-  description = db.Column(db.String(), nullable=False)
-  completed = db.Column(db.Boolean, nullable=False, default=False)
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
-  # we should name the foreign key if we want to be able to downgrade the revision of migration. Otherwise it errors out with sqlalchemy.exc.CompileError that it cannot drop the foreign key constraint since the constraint has no name.
+    # we should name the foreign key if we want to be able to downgrade the revision of migration. Otherwise it errors out with sqlalchemy.exc.CompileError that it cannot drop the foreign key constraint since the constraint has no name.
 
-  # `server_default` vs `default` in SQLAlchemy:
-  # SQLAlchemy uses two different options on Models' Columns to make INSERTions to have default values specified:
-  # `default` works on application server's side. It renders the default expression in the INSERT or UPDATE statements and sends them to the database server. This option doesn't specify anything on the database side.
-  # `server_default` works on database server's side. SQLAlchemy places the places the expression in the CREATE TABLE statement to put a default value there. Even if application server's side does not provide any value during an INSERT or UPDATE request, the database server puts the default there on its own.
-  # read more: https://stackoverflow.com/questions/52431208/sqlalchemy-default-vs-server-default-performance
+    # `server_default` vs `default` in SQLAlchemy:
+    # SQLAlchemy uses two different options on Models' Columns to make INSERTions to have default values specified:
+    # `default` works on application server's side. It renders the default expression in the INSERT or UPDATE statements and sends them to the database server. This option doesn't specify anything on the database side.
+    # `server_default` works on database server's side. SQLAlchemy places the places the expression in the CREATE TABLE statement to put a default value there. Even if application server's side does not provide any value during an INSERT or UPDATE request, the database server puts the default there on its own.
+    # read more: https://stackoverflow.com/questions/52431208/sqlalchemy-default-vs-server-default-performance
 
-  list_id = db.Column(db.Integer(), db.ForeignKey('todolists.id', name='todolists_ref'), nullable=False, server_default='1')
-  def __repr__(self):
-    return f'<Todo {self.id} {self.description} in list {TodoList.query.filter_by(id=self.list_id).first()}>'
+    list_id = db.Column(db.Integer(), db.ForeignKey(
+        'todolists.id', name='todolists_ref'), nullable=False, server_default='1')
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description} in list {TodoList.query.filter_by(id=self.list_id).first()}>'
+
 
 class TodoList(db.Model):
-  __tablename__ = 'todolists'
-  id = db.Column(db.Integer(), primary_key=True)
-  name = db.Column(db.String(), nullable=False)
-  # db.relationship('ModelClassName', backref='this_is_how_the_chid_references_its_parent')
-  todos = db.relationship('Todo', backref='list', lazy=True)
-  def __repr__(self):
-    return f'{self.id}: {self.name}'
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    # db.relationship('ModelClassName', backref='this_is_how_the_child_references_its_parent')
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
+    def __repr__(self):
+        return f'{self.id}: {self.name}'
 # —————————————————————————————————————————————————————————————————————————————
 # HANDLE ROUTES
 # —————————————————————————————————————————————————————————————————————————————
-@app.route('/todos/delete',methods=['DELETE'])
+
+
+@app.route('/todos/delete', methods=['DELETE'])
 def delete_todo():
-  error = False
-  body = {}
-  try:
-    todo_id = request.get_json()['id']
-    todo = Todo.query.get(todo_id) 
-    db.session.delete(todo)
-    db.session.commit()
-    body['id'] = todo.id
-  except:
-    error = True
-    db.session.rollback()
-    print(sys.exc_info())
-  finally:
-    db.session.close()
-  if error:
-    abort (400)
-  else:
-    return jsonify(body)
+    error = False
+    body = {}
+    try:
+        todo_id = request.get_json()['id']
+        todo = Todo.query.get(todo_id)
+        db.session.delete(todo)
+        db.session.commit()
+        body['id'] = todo.id
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
 
 @app.route('/todos/set-completed', methods=['PUT'])
 def set_completed_todo():
-  error = False
-  body = {}
-  try:
-    updateId = request.get_json()['id']
-    isCompleted = request.get_json()['completed']
-    todo = Todo.query.get(updateId)
-    todo.completed = isCompleted
-    db.session.commit()
-    body['id'] = updateId
-    body['completed'] = todo.completed
-  except:
-    error = True
-    db.session.rollback()
-    print(sys.exc_info())
-  finally:
-    db.session.close()
-  if error:
-    abort (400)
-  else:
-    return jsonify(body)
+    error = False
+    body = {}
+    try:
+        updateId = request.get_json()['id']
+        isCompleted = request.get_json()['completed']
+        todo = Todo.query.get(updateId)
+        todo.completed = isCompleted
+        db.session.commit()
+        body['id'] = updateId
+        body['completed'] = todo.completed
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  error = False
-  body = {}
-  try:
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    body['id'] = todo.id
-    body['completed'] = todo.completed
-    body['decription'] = todo.description
-  except:
-    error = True
-    db.session.rollback()
-    print(sys.exc_info())
-  finally:
-    db.session.close()
-  if error:
-    abort (400)
-  else:
-    return jsonify(body)
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['id'] = todo.id
+        body['completed'] = todo.completed
+        body['decription'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
 
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
-  return render_template(
-    'index.html', 
-    lists=TodoList.query.all(), 
-    active_list=TodoList.query.get(list_id),
-    todos=Todo.query.filter_by(list_id=list_id).order_by('completed').order_by(Todo.id.desc()).all())
+    return render_template(
+        'index.html',
+        lists=TodoList.query.all(),
+        active_list=TodoList.query.get(list_id),
+        todos=Todo.query.filter_by(list_id=list_id).order_by('completed').order_by(Todo.id.desc()).all())
+
 
 @app.route('/')
 def index():
-  return redirect(url_for('get_list_todos', list_id=1))
+    return redirect(url_for('get_list_todos', list_id=1))
+
+
 # —————————————————————————————————————————————————————————————————————————————
 # FOR OPTIONAL RUNNING THE APP WITH `PYTHON` COMMAND
 # —————————————————————————————————————————————————————————————————————————————
 if __name__ == '__main__':
-  app.run()
+    app.run()
